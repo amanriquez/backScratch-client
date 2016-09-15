8f1134c85932bba4fa85e08bdaad40a6dd5b9614
Daniel Glaser <glaserd92@gmail.com>
2016-09-15 12:17:16 -0500

    (fix) Style rating form and pass down task name in state

--- src/modules/taskManager/ratingForm/RatingFormView.js
+++ src/modules/taskManager/ratingForm/RatingFormView.js
@@ -3,7 +3,8 @@ import _ from 'underscore';
 import {
   View,
   TouchableOpacity,
-  Text
+  Text,
+  ScrollView
 } from 'react-native';
 import {
   Form,
@@ -12,6 +13,7 @@ import {
   PickerField
 } from 'react-native-form-generator';
 import styles from '../../../styles';
+import colors from '../../../styles/colors';
 import * as RatingFormStateActions from './RatingFormState';
 import * as NavigationStateActions from '../../navigation/NavigationState';
 
@@ -19,6 +21,7 @@ const RatingFormView = React.createClass({
   propTypes: {
     userId: PropTypes.number.isRequired,
     taskId: PropTypes.number.isRequired,
+    taskName: PropTypes.string.isRequired,
     userIsRequestor: PropTypes.bool.isRequired,
     dispatch: PropTypes.func.isRequired
   },
@@ -48,46 +51,47 @@ const RatingFormView = React.createClass({
   },
   render() {
     return (
-      <View style={styles.formContainer}>
-        <Text style={styles.formTextTitle}>Task Completed!</Text>
+      <View style={styles.formRatingContainer}>
+        <Text style={styles.formTextTitle}>{this.props.taskName}</Text>
+        <Text style={{fontSize: 25, color: colors.white, fontWeight: '500'}}>Completed!</Text>
         <Text style={styles.formTextSubTitle}>Please fill out the rating form</Text>
 
-        <View style={styles.detailSeperator} />
+        <View style={styles.longDetailSeperator} />
 
-        <Form ref='taskForm'
-          onFocus={this.handleFormFocus}
-        >
-          <PickerField
-            ref='Rating'
-            label='User Rating'
-            options={{
-              '': '',
-              1: '1',
-              2: '2',
-              3: '3',
-              4: '4',
-              5: '5'
-            }}
-            helpText='Rate the other user. 5 being the best!'
-          />
-          <Separator />
-          <InputField
-            multiline={true}
-            ref='Comment'
-            placeholder='Comments'
-            helpText={'Include comments about your experience with the other user. We will save these comments for quality assurance purposes.'}
-          />
-        </Form>
-
+        <ScrollView style={{backgroundColor: colors.lightPrimaryColor}}>
+          <Form ref='taskForm'
+            onFocus={this.handleFormFocus}
+            style={{backgroundColor: colors.lightPrimaryColor, marginTop: 90}}
+          >
+            <PickerField
+              ref='Rating'
+              label='User Rating'
+              options={{
+                '': '',
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5'
+              }}
+              helpText='Rate the other user. 5 being the best!'
+            />
+            <Separator />
+            <InputField
+              multiline={true}
+              ref='Comment'
+              placeholder='Comments'
+              helpText={'Include comments about your experience with the other user. We will save these comments for quality assurance purposes.'}
+            />
+          </Form>
+        </ScrollView>
+        <View style={styles.longDetailSeperator} />
         <TouchableOpacity
           style={styles.button}
           onPress={this.submitTask}
         >
-          <Text style={styles.buttonText}>
-            Submit
-          </Text>
+          <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
-
       </View>
     );
   }
